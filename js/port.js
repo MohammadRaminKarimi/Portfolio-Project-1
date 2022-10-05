6a8542d90bf287bcfe6dfada43030da1a1d05470
Ramin <karimiramin662@gmail.com>
2022-10-04 21:32:53 -0700

    added port js file

---
+++ js/port.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector(".navbar");
+window.addEventListener('scroll', function() {
+   if (window.scrollY > 100) {
+   	navbar.classList.add('bg-dark');
+   } else {
+   	navbar.classList.remove('bg-dark');
+   }
+});
+
+
+
+let arrowin = document.querySelector('#arrowin');
+window.addEventListener("click", function () {
+   window.scrollTo(0, 0);
+});
