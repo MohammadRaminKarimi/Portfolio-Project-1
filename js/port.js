bd293c6f86943a0c940605efb2bf4f155bcbbff0
Ramin <karimiramin662@gmail.com>
2022-12-05 13:27:45 -0800

    update style css file

--- js/port.js
+++ js/port.js
@@ -19,3 +19,15 @@ let arrowin = document.querySelector('#arrowin');
 window.addEventListener("click", function () {
    window.scrollTo(0, 0);
 });
+
+
+
+let popup = document.getElementById("popup");
+
+function openPopup() {
+   popup.classList.add("open-pop");
+}
+
+function closePopup() {
+   popup.classList.remove("open-pop");
+}
